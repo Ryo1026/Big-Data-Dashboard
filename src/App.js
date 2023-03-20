fbcc83397b9562f28da32ecd9065fc12ad20bb0e
Ryo1026 <c9pcv5nvbp@gmail.com>
2023-03-20 23:36:12 +0800

    Implement nav and left content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import gear from "./Vector.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <span className="nav-title">LOGO</span>
+        <div className="setting">
+          <img src={gear} alt="gear" />
+        </div>
       </header>
+      <div className="container">
+        <div className="taiwan">TAIWAN</div>
+        <div className="content"></div>
+      </div>
     </div>
   );
 }
